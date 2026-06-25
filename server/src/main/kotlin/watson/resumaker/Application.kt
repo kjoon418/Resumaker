db5002b8cff7b5f4648a01e1cbb901bd33dece58
Kim, Joon <kjoon418@naver.com>
2026-06-25 09:50:59 +0900

    refactor: 불필요한 주석 제거

--- server/src/main/kotlin/watson/resumaker/Application.kt
+++ server/src/main/kotlin/watson/resumaker/Application.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-// @EnableScheduling: 비동기 생성 작업 워커(GenerationJobWorker)·목표 작성전략 워커의 @Scheduled 폴링을 활성화한다.
 @SpringBootApplication
 @EnableScheduling
 class ResumakerApplication
